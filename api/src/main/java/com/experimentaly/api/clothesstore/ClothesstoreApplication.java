d603bc6fd91205ecb7ade778c56fdc5d2100ea63
Juan Stiven Muñoz Garcia <juan.98mg@gmail.com>
2021-09-04 09:55:06 -0500

    creating bean modelMapper

--- api/src/main/java/com/experimentaly/api/clothesstore/ClothesstoreApplication.java
+++ api/src/main/java/com/experimentaly/api/clothesstore/ClothesstoreApplication.java
@@ -1,13 +1,20 @@
 package com.experimentaly.api.clothesstore;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ClothesstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClothesstoreApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
 	}
 
 }
