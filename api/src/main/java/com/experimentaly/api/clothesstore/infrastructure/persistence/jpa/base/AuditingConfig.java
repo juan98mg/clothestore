5bfaad655c85de1300f23ef2a04f35598fc4b032
Juan Stiven Muñoz Garcia <juan.98mg@gmail.com>
2021-09-04 09:57:45 -0500

    adding spring boot configurations

---
+++ api/src/main/java/com/experimentaly/api/clothesstore/infrastructure/persistence/jpa/base/AuditingConfig.java
@@ -0,0 +1,10 @@
+package com.experimentaly.api.clothesstore.infrastructure.persistence.jpa.base;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+
+}
