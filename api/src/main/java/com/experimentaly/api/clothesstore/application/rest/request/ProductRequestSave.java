1e1b2b2477f8ef658b47368f775fe41a1372c30a
Juan Stiven Muñoz Garcia <juan.98mg@gmail.com>
2021-09-04 09:58:09 -0500

    adding request

---
+++ api/src/main/java/com/experimentaly/api/clothesstore/application/rest/request/ProductRequestSave.java
@@ -0,0 +1,31 @@
+package com.experimentaly.api.clothesstore.application.rest.request;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Positive;
+import com.experimentaly.api.clothesstore.core.util.AppConstants;
+import com.experimentaly.api.clothesstore.infrastructure.persistence.jpa.base.Popularity;
+import org.springframework.web.multipart.MultipartFile;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+public class ProductRequestSave {
+
+    @NotBlank(message = AppConstants.PRODUCT_NAME_NOT_VALID)
+    private String name;
+    private String description;
+    @Positive(message = AppConstants.MIN_VALUE_PRICE_VALIDATION_ERROR)
+    private float price;
+    @Positive(message = AppConstants.MIN_VALUE_DISCOUNT_VALIDATION_ERROR)
+    @Max(value = 100, message = AppConstants.MAX_VALUE_DISCOUNT_VALIDATION_ERROR)
+    private float discount;
+    private Popularity popularity = Popularity.ZERO;
+    @NotBlank(message = AppConstants.PRODUCT_COUNTRY_MUST_NOT_BE_NULL)
+    private String selledCountry;
+    private MultipartFile[] files;
+
+
+}
