0548ea57afb98df6f221886eaf68743bd9ef138f
Juan Stiven Muñoz Garcia <juan.98mg@gmail.com>
2021-09-05 20:12:21 -0500

    adding constants

---
+++ app/src/app/util/app_constants.ts
@@ -0,0 +1,14 @@
+export const AppConstants = {
+  siteNotAvaliabled: function (option: string) {
+    return `Lo sentimos!!.  ${option} está en construcción`;
+  },
+  sweetIcons: {
+    success: 'success',
+    error: 'error',
+    warning: 'warning',
+    info: 'info',
+    question: 'question',
+  },
+  building: 'assets/icons/building.gif',
+  titleSorry: 'Lo sentimos!',
+};
